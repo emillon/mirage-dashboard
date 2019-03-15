0ddc79627d0f0f59e694f990e43c815639718e7b
Etienne Millon <me@emillon.org>
2019-03-15 16:00:42 +0100

    Display latest Mirage PRs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,33 @@
+require("dotenv").config({
+  path: `.env`,
+})
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Mirage dashboard`,
+    description: `This displays info about Mirage packages`,
+    author: `@emillon`,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-graphql`,
+      options: {
+        typeName: `ONEGRAPH`,
+        fieldName: `oneGraph`,
+        url: `https://serve.onegraph.com/dynamic?app_id=${
+          process.env.ONEGRAPH_APPID
+        }`,
+        headers: {
+          Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -15,20 +37,5 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // 'gatsby-plugin-offline',
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,91 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql } from "gatsby"
+import Container from "../components/container"
+import PrTable from "../components/pr-table"
+import moment from "moment"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+const byClosedAt = (a, b) => {
+  const da = new Date(a.closedAt)
+  const db = new Date(b.closedAt)
+  return (da < db) - (da > db)
+}
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const isRecent = pr => moment(pr.closedAt).isAfter(moment().subtract(1, "week"))
+
+function flatten(a) {
+  return [].concat.apply([], a)
+}
+
+class IndexPage extends React.Component {
+  repos() {
+    const gh = this.props.data.oneGraph.gitHub
+    return flatten([
+      gh.mirage.repositories.nodes,
+      [gh.g2p.wodan, gh.mirleft.asn1c, gh.mirleft.tls, gh.mirleft.x509],
+    ])
+  }
+
+  prs() {
+    const repos = this.repos()
+    const prs = flatten(repos.map(repo => repo.pullRequests.nodes)).filter(
+      isRecent
+    )
+    prs.sort(byClosedAt)
+    return prs
+  }
+
+  render() {
+    return (
+      <Container>
+        <h1>Latest PRs</h1>
+        <PrTable prs={this.prs()} />
+      </Container>
+    )
+  }
+}
 
 export default IndexPage
+
+export const query = graphql`
+  query repos {
+    oneGraph {
+      gitHub {
+        mirage: organization(login: "mirage") {
+          repositories(first: 20) {
+            nodes {
+              ...prs
+            }
+          }
+        }
+        g2p: user(login: "g2p") {
+          wodan: repository(name: "wodan") {
+            ...prs
+          }
+        }
+        mirleft: organization(login: "mirleft") {
+          asn1c: repository(name: "ocaml-asn1-combinators") {
+            ...prs
+          }
+          tls: repository(name: "ocaml-tls") {
+            ...prs
+          }
+          x509: repository(name: "ocaml-x509") {
+            ...prs
+          }
+        }
+      }
+    }
+  }
+
+  fragment prs on ONEGRAPH_GitHubRepository {
+    pullRequests(
+      states: [MERGED]
+      first: 20
+      orderBy: { field: UPDATED_AT, direction: DESC }
+    ) {
+      nodes {
+        ...PrFragment
+      }
+    }
+  }
+`
